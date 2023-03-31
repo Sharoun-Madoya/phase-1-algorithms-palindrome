75be2f2a809b87b0bc6193989a1eeacf084cf900
[sharoun <[madoyasharoun@gmail.com]>
2023-03-31 17:00:30 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = word.split('').reverse().join('')
+  if(word === reversedWord){
+    return true;
+  }else 
+  return false
 }
-
+// function isPalindrome(word){
+//   let reversedWord = '';
+//   for (const character of word){
+//     reversedWord= character +reversedWord
+//   }
+// }
 /* 
   Add your pseudocode here
 */
@@ -9,6 +19,13 @@ function isPalindrome(word) {
 /*
   Add written explanation of your solution here
 */
+// using for loop 
+// make sure length of array are same 
+// make sure beginnig and end pont are same, 
+// if true compare until beggin and end point are equal
+// or beginnigis > than end point for words that are not even 
+// if false return false /if true = pseudocode 
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
